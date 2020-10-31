19045035e5dee53ab5d3479a6e68d271592130eb
Wellington Wallace <wellingtonwallace@gmail.com>
2020-10-31 20:25:20 -0300

    creating a user interface

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -1,7 +1,6 @@
 #ifndef CPU_HPP
 #define CPU_HPP
 
-#include <gtkmm/adjustment.h>
 #include <gtkmm/builder.h>
 #include <gtkmm/comboboxtext.h>
 #include <gtkmm/flowbox.h>
@@ -28,18 +27,11 @@ class Cpu : public Gtk::Grid {
 
   Application* app = nullptr;
 
-  Gtk::Switch* use_sched_batch = nullptr;
+  Gtk::Switch *use_sched_batch = nullptr, *child_runs_first = nullptr;
 
   Gtk::ComboBoxText* frequency_governor = nullptr;
 
   Gtk::FlowBox* affinity_flowbox = nullptr;
-
-  // Glib::RefPtr<Gtk::Adjustment> power_cap;
-
-  void find_hwmon_index();
-  void read_power_cap();
-  void read_power_cap_max();
-  void read_performance_level();
 };
 
 #endif
--- src/cpu.cpp
+++ src/cpu.cpp
@@ -14,10 +14,9 @@ Cpu::Cpu(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& builder, App
   // loading glade widgets
 
   builder->get_widget("use_sched_batch", use_sched_batch);
+  builder->get_widget("child_runs_first", child_runs_first);
   builder->get_widget("frequency_governor", frequency_governor);
   builder->get_widget("affinity_flowbox", affinity_flowbox);
-
-  // power_cap = Glib::RefPtr<Gtk::Adjustment>::cast_dynamic(builder->get_object("power_cap"));
 
   // initializing widgets
 
@@ -46,9 +45,8 @@ Cpu::Cpu(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& builder, App
 
   frequency_governor->set_active_text(selected_governor);
 
-  // signals connection
-
-  // button_add->signal_clicked().connect([=]() { liststore->append(); });
+  child_runs_first->set_active(
+      static_cast<bool>(std::stoi(util::read_system_setting("/proc/sys/kernel/sched_child_runs_first")[0])));
 }
 
 Cpu::~Cpu() {
@@ -63,89 +61,4 @@ void Cpu::add_to_stack(Gtk::Stack* stack, Application* app) {
   builder->get_widget_derived("widgets_grid", ui, app);
 
   stack->add(*ui, "cpu", _("CPU"));
-}
-
-void Cpu::find_hwmon_index() {
-  auto path = fs::path("/sys/class/drm/card/device/hwmon/");
-
-  for (const auto& entry : fs::directory_iterator(path)) {
-    auto child_directory = fs::path(entry).filename().string();
-
-    // It is unlikely that a system has more than 10 hwmon devices so we just get the last character
-
-    std::string index_str(1, child_directory.back());
-
-    util::debug(log_tag + "hwmon device index: " + index_str);
-  }
-}
-
-void Cpu::read_power_cap() {
-  auto path = fs::path("/sys/class/drm/card/power1_cap");
-
-  if (!fs::is_regular_file(path)) {
-    util::debug(log_tag + "file " + path.string() + " does not exist!");
-    util::debug(log_tag + "could not change the power cap!");
-  } else {
-    std::ifstream f;
-
-    f.open(path, std::ios::in);
-
-    int raw_value = 0;  // microWatts
-
-    f >> raw_value;
-
-    f.close();
-
-    int power_cap_in_watts = raw_value / 1000000;
-
-    // power_cap->set_value(power_cap_in_watts);
-
-    util::debug(log_tag + "current power cap: " + std::to_string(power_cap_in_watts) + " W");
-  }
-}
-
-void Cpu::read_power_cap_max() {
-  auto path = fs::path("/sys/class/drm/card/power1_cap_max");
-
-  if (!fs::is_regular_file(path)) {
-    util::debug(log_tag + "file " + path.string() + " does not exist!");
-    util::debug(log_tag + "could not read the maximum power cap!");
-  } else {
-    std::ifstream f;
-
-    f.open(path, std::ios::in);
-
-    int raw_value = 0;  // microWatts
-
-    f >> raw_value;
-
-    f.close();
-
-    int power_cap_in_watts = raw_value / 1000000;
-
-    // power_cap->set_upper(power_cap_in_watts);
-
-    util::debug(log_tag + "maximum allowed power cap: " + std::to_string(power_cap_in_watts) + " W");
-  }
-}
-
-void Cpu::read_performance_level() {
-  auto path = fs::path("/sys/class/drm/card/device/power_dpm_force_performance_level");
-
-  if (!fs::is_regular_file(path)) {
-    util::debug(log_tag + "file " + path.string() + " does not exist!");
-    util::debug(log_tag + "could not change the performance level!");
-  } else {
-    std::ifstream f;
-
-    f.open(path, std::ios::in);
-
-    std::string level;
-
-    f >> level;
-
-    f.close();
-
-    util::debug(log_tag + "current performance level: " + level);
-  }
 }

c818b3e86b76fb34207dfe6719c30fe88cee676e
wwmm <wellingtonwallace@gmail.com>
2024-04-23 02:08:44 -0300

    making sure there is only one instance running

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <kaboutdata.h>
 #include <klocalizedcontext.h>
+#include <qlockfile.h>
 #include <qobject.h>
 #include <qqml.h>
 #include <qqmlapplicationengine.h>
 #include <qquickstyle.h>
+#include <qstandardpaths.h>
 #include <qstringliteral.h>
 #include <qtenvironmentvariables.h>
 #include <qurl.h>
@@ -11,8 +13,42 @@
 #include <KLocalizedString>
 #include <QApplication>
 #include <QtQml>
+#include <memory>
 #include "config.h"
 #include "presets_manager.hpp"
+#include "util.hpp"
+
+auto get_lock_file() -> std::unique_ptr<QLockFile> {
+  auto lockFile = std::make_unique<QLockFile>(QString::fromStdString(
+      QStandardPaths::writableLocation(QStandardPaths::TempLocation).toStdString() + "/fastgame.lock"));
+
+  lockFile->setStaleLockTime(0);
+
+  bool status = lockFile->tryLock(100);
+
+  if (!status) {
+    util::critical("Could not lock the file: " + lockFile->fileName().toStdString());
+
+    switch (lockFile->error()) {
+      case QLockFile::NoError:
+        break;
+      case QLockFile::LockFailedError: {
+        util::critical("Another instance already has the lock");
+        break;
+      }
+      case QLockFile::PermissionError: {
+        util::critical("No permission to reate the lock file");
+        break;
+      }
+      case QLockFile::UnknownError: {
+        util::critical("Unknown error");
+        break;
+      }
+    }
+  }
+
+  return lockFile;
+}
 
 void construct_about_window() {
   KAboutData aboutData(
@@ -39,6 +75,12 @@ void construct_about_window() {
 }
 
 int main(int argc, char* argv[]) {
+  auto lockFile = get_lock_file();
+
+  if (!lockFile->isLocked()) {
+    return -1;
+  }
+
   QApplication app(argc, argv);
 
   KLocalizedString::setApplicationDomain(APPLICATION_DOMAIN);
